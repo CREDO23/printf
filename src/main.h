098696427fbb6a7ca4aa95d1b1defe3c38a78a24
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-25 13:15:02 -0700

    add-putchar_function

--- src/main.h
+++ src/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int __putchar(char c);
+char *converter(unsigned int num, int base);
+
 
 #endif
