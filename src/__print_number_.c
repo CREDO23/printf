782110f7a904814904645d5a4554fd00813ecb7e
CREDO23 <thierrybakera12@gmail.com>
2023-03-26 21:56:31 +0200

    add the function to print number

--- src/__print_number_.c
+++ src/__print_number_.c
@@ -1,51 +1,53 @@
 #include "main.h"
 
-
 /**
- * converter - A converter [10 - 8] [10 - 2] [10 - 16]  , [10 - ...]
+ * __print_number__ - A converter [10 - 8] [10 - 2] [10 - 16]  , [10 - ...]
  * @num: The number to convert
  * @base: The base in which we'll convert @num
+ * @specifier: The conversion sppecifier
  *
  * Return: The @num converted
  */
-
-
-char *converter(unsigned int num, int base)
+char *__print_number__(int num, int base, char specifier)
 {
-
 	char rep[] = {'0', '1', '2', '3', '4', '5',
-			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
+			'6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 	char *res = malloc(sizeof(int) + 4);
-
 	char *tmp = malloc(sizeof(int) + 4);
-
 	int i = 0;
-
 	int len = 0;
+	int ngtv = 0;
+
+	if (specifier != 'u' && num < 0)
+	{
+		num = -num;
+		ngtv = 1;
+	}
 
 	do {
 		tmp[i++] = rep[num % base];
-
 		num /= base;
-
 	} while (num != 0);
 
+	if (specifier != 'u' && ngtv)
+	{
+		tmp[i++] = '-';
+	}
 
 	len = i - 1;
-
 	i = 0;
-
 
 	while (len >= 0)
 	{
 		res[len--] = tmp[i++];
 	}
 
-
 	res[i] = '\0';
-
 	free(tmp);
 
-	return (res);
+	while (*res != '\0')
+	{
+		_putchar(*res);
+		res++;
+	}
 }
