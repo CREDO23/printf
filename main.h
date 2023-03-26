25062168cd61e08350e43b253b35fcec1bab6472
CREDO23 <thierrybakera12@gmail.com>
2023-03-26 23:17:04 +0200

    fix

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-void __print_number__( int num, unsigned int base, char specifier );
+void __print_number__(int num, unsigned int base, char specifier);
 void __print_string__(char *str, char c, char specifier);
 char __format_tracker__(char c);
 int _printf(const char *format, ...);
