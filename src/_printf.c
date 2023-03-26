846f16a4edc9e62316b72813e5fcd8e2786ca667
CREDO23 <thierrybakera12@gmail.com>
2023-03-26 22:36:04 +0200

    add the _printf funtion

---
+++ src/_printf.c
@@ -0,0 +1,48 @@
+/**
+ * _printf - The printf funtion
+ * @format: The string which content specifiers
+ *
+ * Return: The numer of characters printed
+ */
+int _printf(char *format, ...)
+{
+		int i = 0;
+		int nchr = 0;
+		va_list ap;
+		int tmp;
+
+		va_start(ap, format);
+	while (*format != '\0')
+	{
+	if (*format != '%' || (*format == '%' && *(format + 1) == '%'))
+	{
+		_putchar(*format);
+		nchr++;
+	} else if (*format == '%' && __format_tracker__(*(format + 1)))
+	{
+		if (__format_tracker__(*(format + 1)) == 's')
+		{
+			if (*(format + 1) == 's')
+			{
+				char *ptr = va_arg(ap, char*);
+
+				__print_string__(ptr, '\0', *(format + 1));
+			}
+			if (*(format + 1) == 'c')
+			{
+				char c = va_arg(ap, int);
+
+				__print_string__("", c, *(format + 1));
+			}
+		}
+		if (__format_tracker__(*(format + 1)) == 'i')
+		{
+			__print_number__(va_arg(ap, int), 10, *(format + 1));
+		}
+			format++;
+	}
+		format++;
+	}
+	va_end(ap);
+	return (nchr);
+}
